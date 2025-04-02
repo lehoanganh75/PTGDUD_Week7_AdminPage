dc24f38814e123b58d919b881b8dabe2f8bd0895
H82M19\Student <Student>
2025-04-02 19:28:56 +0700

    gridcss

--- adminpage/src/App.tsx
+++ adminpage/src/App.tsx
@@ -1,34 +1,42 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
+import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div className="container h-100vh grid gap-1 p-2 m-1 ml-6">
+        <div className="header">
+          <h2>My Header</h2>
+        </div>
+        <div className="menu">
+          <div>
+            <img src="./image/logo.png" alt="" />
+          </div>
+          <div>
+          <ul>
+            <li className='flex'>
+              <img src="./image/Folder.png" alt="" className='mr-2'/>
+              <a href="#">Dashboard</a>
+              </li>
+            <li className='flex'>
+            <img src="./image/Folder.png" alt="" className='mr-2'/>
+              <a href="#">Projects</a>
+              </li>
+            <li className='flex'><img src="./image/Folder.png" alt="" className='mr-2'/><a href="#">Teams</a></li>
+            <li className='flex'><img src="./image/Folder.png" alt="" className='mr-2' /><a href="#">Analytics</a></li>
+            <li className='flex' ><img src="./image/Folder.png" alt="" className='mr-2'/><a href="#">Messages</a></li>
+            <li className='flex'><img src="./image/Folder.png" alt="" className='mr-2'/><a href="#">Integrations</a></li>
+          </ul>
+          </div>
+        </div>
+        <div className="content"><h3>Lorem Ipsum</h3><p>Lorem ipsum odor amet, consectetuer adipiscing elit. Ridiculus sit nisl laoreet facilisis aliquet. Potenti dignissim litora eget montes rhoncus sapien neque urna. Cursus libero sapien integer magnis ligula lobortis quam ut.</p></div>
+        <div className="footer"><h4>Footer</h4></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
